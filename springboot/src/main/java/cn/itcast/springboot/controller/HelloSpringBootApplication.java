90e6c31bf0dd6cf50b0961b394e7ddc786fb73c8
xcm31725 <xcm31725@qq.com>
2018-06-12 22:22:35 +0800

    springboot introduction demo

---
+++ springboot/src/main/java/cn/itcast/springboot/controller/HelloSpringBootApplication.java
@@ -0,0 +1,18 @@
+package cn.itcast.springboot.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+    public static void main(String[] args) {
+        //启动应用
+        SpringApplication.run(HelloSpringBootApplication.class,args);
+    }
+}
+
+
+
+
+
+
